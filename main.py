b69e08c91b692914d7b508b2ed43ea51761a6ba8
Ceiligirl72 <ceiligirl72@gmail.com>
2017-02-14 12:39:56 -0600

    complete but with 1 issue - encrpyted content disappears

--- main.py
+++ main.py
@@ -16,19 +16,33 @@
 #
 import webapp2
 import caesar
+import cgi
+
+def build_page(textarea_content):
+    rot_label = "<label>Rotate By </label>"
+    rotation_input = "<input type='number' name='rotation'/>"
+    messsage_label = "<label>Text to Encrypt </label>"
+    textarea = "<textarea name='message'></textarea>"
+    submit = "<input type='submit'/>"
+    header = "<h2>Web Caesar</h2>"
+
+    form = ("<form method='post'>" +  rot_label + rotation_input
+    + "<br>"+ messsage_label + textarea + "<br>" + submit + "</form>")
+
+    return header + form
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        content = build_page("")
+        self.response.write(content)
 
-
-        message = "Encrypt This T3xt!"
-        encrypted_message = (caesar.encrypt(message, 13))
-        submit_button = "<input type= 'submit'>"
-        instruction = "Enter some text to Encrypt:  " + "<br>"
-        form = "<form>" + "<textarea>" + encrypted_message + "</textarea>" + "<br>" +submit_button +"</form>"
-
-        self.response.write(instruction + form)
-
+    def post(self):
+        message = self.request.get("message")
+        rotation = int(self.request.get("rotation"))
+        encrypted_message = caesar.encrypt(message, rotation)
+        escaped_msg = cgi.escape(encrypted_message)
+        content = build_page(escaped_msg)
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
